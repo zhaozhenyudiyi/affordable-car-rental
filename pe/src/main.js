0e5ea5cc6ddbaa3bef62448e89b0c35c1b0cf882
zhaozhenyu <1063662181@qq.com>
2019-07-11 17:02:44 +0800

    依赖安装完成

--- pe/src/main.js
+++ pe/src/main.js
@@ -5,9 +5,11 @@ import App from './App';
 import router from './router';
 
 
-import Swiper from 'swiper'; 
-import 'swiper/dist/css/swiper.min.css';
-Vue.use(Swiper);
+
+
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper)
 
 
 import Mint from 'mint-ui';
@@ -16,7 +18,7 @@ Vue.use(Mint);
 
 import Axios from 'axios';
 import VueAxios from 'vue-axios';
-Vue.use(VueAxios, Axios);
+Vue.use(VueAxios, Axios)
 
 
 import Vant from 'vant';
--- pe/src/router/index.js
+++ pe/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import index from '../components/index/index'
+import index from '../components/zzy/start/start'
 
 Vue.use(Router)
 
