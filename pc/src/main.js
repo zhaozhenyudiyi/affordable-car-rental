6bcb4cee0b6249601d3d6ade88c862ee1b339c88
zhaozhenyu <1063662181@qq.com>
2019-07-11 15:24:19 +0800

    依赖安装完成

--- pc/src/main.js
+++ pc/src/main.js
@@ -1,8 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+// import Swiper from 'swiper'; 
+// import Mint from 'mint-ui';
+// import Vant from 'vant';
+ 
+
+// import 'swiper/dist/css/swiper.min.css';
+// import 'mint-ui/lib/style.css'; 
+// import 'vant/lib/vant-css/index.css';
+
+Vue.use(VueAxios, axios)
+// Vue.use(Mint);
+// Vue.use(Vant);
+// Vue.use(Swiper);
 
 Vue.config.productionTip = false
 
