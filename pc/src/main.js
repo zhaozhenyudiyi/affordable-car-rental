f42b14b9f62dffdd8e8adbace2ef9c2fc80738a9
‘mengyuntao123’ <1723421396@qq.com>
2019-07-17 11:47:26 +0800

    测试

--- pc/src/main.js
+++ pc/src/main.js
@@ -3,20 +3,15 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
-<<<<<<< HEAD
 // import Vue from 'vue';
-=======
->>>>>>> upstream/master
 import axios from 'axios';
 import VueAxios from 'vue-axios';
-<<<<<<< HEAD
 
 // import ElementUI from 'element-ui';
 // import 'element-ui/lib/theme-chalk/index.css';
 // Vue.use(ElementUI);
 Vue.use(VueAxios, axios)
 
-=======
 Vue.use(VueAxios, axios)
 //ElementUI
 import ElementUI from 'element-ui';
@@ -24,7 +19,6 @@ import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
 
->>>>>>> zhaozhenyudiyi/master
 
 import echarts from 'echarts'
 Vue.prototype.$echarts = echarts
