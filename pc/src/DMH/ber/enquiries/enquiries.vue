a00cac76bd1e9b1338367add32f4895cb7a72251
212552505@qq.com <212552505@qq.com>
2019-07-17 08:51:48 +0800

    丁明辉不知道第几次上传

--- pc/src/DMH/ber/enquiries/enquiries.vue
+++ pc/src/DMH/ber/enquiries/enquiries.vue
@@ -7,7 +7,7 @@
     </div>
     <aside v-if="show">
       <el-table
-        :data="tableData.slice((currentPage-1)*pagesize,currentPage*pagesize)"
+        :data="table.slice((currentPage-1)*pagesize,currentPage*pagesize)"
         style="width: 100%"
         :header-cell-style="{background:'#E8E8E8'}"
         @cell-click="fn"
@@ -33,14 +33,14 @@
         <h3>会员基本信息</h3>
         <div class="one">
           <div class="two">
-            <p>客户姓名：马**</p>
-            <p>联系方式：130****3964</p>
-            <p>平驾币：2000</p>
+            <p>客户姓名：{{xq.Basic_name}}</p>
+            <p>联系方式：{{xq.Basic_phone}}</p>
+            <p>平驾币：{{ xq.Basic_currency }}</p>
           </div>
           <div class="free">
-            <p>证件类型：二代身份证</p>
-            <p>证件号码：41262********1259</p>
-            <p>积分：100</p>
+            <p>证件类型：{{xq.Basic_type}}</p>
+            <p>证件号码：{{xq.Basic_number}}</p>
+            <p>积分：{{ xq.Basic_integral}}</p>
           </div>
         </div>
       </div>
@@ -48,22 +48,22 @@
         <h3>消费记录</h3>
         <div class="one">
           <div class="two">
-            <p>时间：2019.05.05</p>
-            <p>日租价：￥800</p>
-            <p>定金：￥2000</p>
-            <p>手续费：￥200</p>
-            <p>车辆损失费：无</p>
-            <p>总费用：￥3000</p>
+            <p>时间：{{xq.ption_timer}}</p>
+            <p>日租价：{{xq.ption_day}}</p>
+            <p>定金：{{xq.ption_Deposit}}</p>
+            <p>手续费：{{xq.ption_Charge}}</p>
+            <p>车辆损失费：{{xq.ption_loss}}</p>
+            <p>总费用：{{xq.ption_cost}}</p>
           </div>
           <div class="free">
-            <p>车辆型号：劳斯莱斯幻影</p>
-            <p>车牌号码：豫F-Z7776</p>
-            <p>租用方式：时租</p>
-            <p>车型：高级</p>
-            <p>取车时间：2019.05.06 8:00</p>
-            <p>还车时间：2019.07.06 8:00</p>
-            <p>实际还车时间：2019.07.06 6：00</p>
-            <p>车辆损失说明：无</p>
+            <p>车辆型号：{{xq.ption_Model}}</p>
+            <p>车牌号码：{{xq.ption_mode}}</p>
+            <p>租用方式：{{xq.ption_type}}</p>
+            <p>车型：{{xq.ption_type}}</p>
+            <p>取车时间：{{xq.ption_car}}</p>
+            <p>还车时间：{{xq.ption_rac}}</p>
+            <p>实际还车时间：{{xq.ption_Actual}}</p>
+            <p>车辆损失说明：{{xq.ption_nothing}}</p>
           </div>
         </div>
       </div>
@@ -71,14 +71,14 @@
         <h3>充值记录</h3>
         <div class="one">
           <div class="two">
-            <p>充值时间：2019.05.01</p>
-            <p>充值金额：￥50</p>
-            <p>会员期限：两个月</p>
+            <p>充值时间：{{xq.charge_timer}}</p>
+            <p>充值金额：{{xq.charge_money}}</p>
+            <p>会员期限：{{xq.charge_term}}</p>
           </div>
           <div class="free">
-            <p>到期时间2019.07.01</p>
-            <p>会员等级：白金</p>
-            <p>会员状态：连续续费</p>
+            <p>到期时间{{xq.charge_ti}}</p>
+            <p>会员等级：{{xq.charge_Grade}}</p>
+            <p>会员状态：{{xq.charge_state}}</p>
           </div>
         </div>
       </div>
@@ -97,43 +97,43 @@ export default {
       currentPage: 1, //默认开始页面
       tableData: [
         {
-          id: "16620194993",
+          id: "1",
           timer: "1999-03-05 19:03:95",
           money: "￥1000",
           mode: "支付宝",
           curr: "1000",
           currency: "10000",
           //会员基本信息
-          // Basic_name: "马先生",
-          // Basic_phone: 13827452207,
-          // Basic_currency: 2000,
-          // Basic_type: 二代身份证,
-          // Basic_number: 8978969865287658,
-          // Basic_integral: 40000,
+          Basic_name: "马先生",
+          Basic_phone: 13827452207,
+          Basic_currency: 2000,
+          Basic_type: "二代身份证",
+          Basic_number: 8978969865287658,
+          Basic_integral: 40000,
           //消费记录
-          // ption_timer: "1898-09-09 23:44:99",
-          // ption_day: "￥800",
-          // ption_Deposit: "￥2000",
-          // ption_Charge: "￥9000",
-          // ption_loss: "0",
-          // ption_cost: "￥9000",
-          // ption_Model: "五菱宏光",
-          // ption_mode: "日租",
-          // ption_type: "高级",
-          // ption_car: "1898-09-09 23:44:99",
-          // ption_rac: "1898-09-09 23:44:99",
-          // ption_Actual: "1898-09-09 23:44:99",
-          // ption_nothing: "无",
+          ption_timer: "1898-09-09 23:44:99",
+          ption_day: "￥800",
+          ption_Deposit: "￥2000",
+          ption_Charge: "￥9000",
+          ption_loss: "0",
+          ption_cost: "￥9000",
+          ption_Model: "五菱宏光",
+          ption_mode: "日租",
+          ption_type: "高级",
+          ption_car: "1898-09-09 23:44:99",
+          ption_rac: "1898-09-09 23:44:99",
+          ption_Actual: "1898-09-09 23:44:99",
+          ption_nothing: "无",
           //充值记录
-          // charge_timer: "1898-09-09 23:44:99",
-          // charge_money: "￥900",
-          // charge_term: "2个月",
-          // charge_ti: "1898-09-09 23:44:99",
-          // charge_Grade: "白金",
-          // charge_state: "连续续期"
+          charge_timer: "1898-09-09 23:44:99",
+          charge_money: "￥900",
+          charge_term: "2个月",
+          charge_ti: "1898-09-09 23:44:99",
+          charge_Grade: "白金",
+          charge_state: "连续续期"
         },
         {
-          id: "16620194993",
+          id: "2",
           timer: "1999-03-05 19:03:95",
           money: "￥1000",
           mode: "支付宝",
@@ -165,7 +165,7 @@ export default {
           currency: "10000"
         },
         {
-          id: "16620194993",
+          id: "3",
           timer: "1999-03-05 19:03:95",
           money: "￥1000",
           mode: "支付宝",
@@ -181,7 +181,7 @@ export default {
           currency: "10000"
         },
         {
-          id: "16620194993",
+          id: "4",
           timer: "1999-03-05 19:03:95",
           money: "￥1000",
           mode: "支付宝",
@@ -189,7 +189,7 @@ export default {
           currency: "100000000000"
         },
         {
-          id: "16620194993",
+          id: "5",
           timer: "1999-03-05 19:03:95",
           money: "￥1000",
           mode: "支付宝",
@@ -197,7 +197,7 @@ export default {
           currency: "10000"
         },
         {
-          id: "16620194993",
+          id: "6",
           timer: "1999-03-05 19:03:95",
           money: "￥1000",
           mode: "支付宝",
@@ -205,7 +205,7 @@ export default {
           currency: "10000"
         },
         {
-          id: "16620194993",
+          id: "7",
           timer: "1999-03-05 19:03:95",
           money: "￥1000",
           mode: "支付宝",
@@ -213,7 +213,7 @@ export default {
           currency: "10000"
         },
         {
-          id: "16620194993",
+          id: "8",
           timer: "1999-03-05 19:03:95",
           money: "asdff",
           mode: "支付宝",
@@ -221,7 +221,7 @@ export default {
           currency: "10000"
         },
         {
-          id: "16620194993",
+          id: "90",
           timer: "1999-03-05 19:03:95",
           money: "￥1000",
           mode: "sdfdfs",
@@ -229,7 +229,7 @@ export default {
           currency: "10000"
         },
         {
-          id: "16620194993",
+          id: "44",
           timer: "adsdf-03-05 19:03:95",
           money: "￥1000",
           mode: "支付宝",
@@ -237,7 +237,7 @@ export default {
           currency: "10000"
         },
         {
-          id: "16620194993",
+          id: "673",
           timer: "1999-03-05 19:03:95",
           money: "￥1000",
           mode: "dfdf",
@@ -245,7 +245,7 @@ export default {
           currency: "10000"
         },
         {
-          id: "16620194993",
+          id: "66",
           timer: "1999-03-05 19:03:95",
           money: "sfsdf",
           mode: "支付宝",
@@ -253,7 +253,7 @@ export default {
           currency: "10000"
         },
         {
-          id: "16620194993",
+          id: "85674",
           timer: "1999-03-05 19:03:95",
           money: "￥1000",
           mode: "支付宝",
@@ -261,7 +261,7 @@ export default {
           currency: "10000"
         },
         {
-          id: "12345678",
+          id: "6765",
           timer: "dfdfd-03-05 19:03:95",
           money: "￥1000",
           mode: "支付宝0000",
@@ -269,7 +269,7 @@ export default {
           currency: "10000"
         },
         {
-          id: "16620194993",
+          id: "4622",
           timer: "1999-03-05 19:03:95",
           money: "￥1000",
           mode: "dfsdf",
@@ -277,7 +277,7 @@ export default {
           currency: "10000"
         },
         {
-          id: "16620194993",
+          id: "566",
           timer: "1999-03-05 19:03:95",
           money: "￥1000",
           mode: "weix",
@@ -285,7 +285,7 @@ export default {
           currency: "10000"
         },
         {
-          id: "16620194993",
+          id: "5346",
           timer: "1999-03-05 19:03:95",
           money: "￥1000",
           mode: "weix",
@@ -293,37 +293,53 @@ export default {
           currency: "10000"
         }
       ],
+      table: [],
       //切换页面
-      show: true
+      show: true,
+      //点开一行拿到的数据
+      xq: null
     };
   },
   methods: {
+    //点击增加
     next() {
       this.currentPage = this.currentPage + 1;
     },
+    //点击减少
     prev() {
       this.currentPage = this.currentPage - 1;
     },
+    //点击页数
     handleCurrentChange(val) {
       this.currentPage = val;
     },
     vip() {
-      this.tableData.forEach(element => {
-        if (element.id === this.input) {
-          this.tableData.unshift(element);
-        }
-      });
+      this.table = [];
+      if (this.input != "") {
+        this.tableData.forEach((a, b, c) => {
+          if (a.id.indexOf(this.input) != -1) {
+            this.table.push(a);
+          }
+        });
+      } else {
+        return;
+      }
     },
-    vip_(){
+    vip_() {
       this.show = true;
     },
     //点击某一行获取其中的数据
     fn(v) {
       this.show = false;
-      console.log(v);
+      this.xq = v;
     }
   },
-  components: {}
+  components: {},
+  mounted() {
+    this.tableData.forEach((a, b, c) => {
+      this.table.push(a);
+    });
+  },
 };
 </script>
 
@@ -331,12 +347,13 @@ export default {
 section {
   width: 100%;
   height: 100%;
+  font-size: 0;
   .ries {
     width: 100%;
     height: 62px;
     font-size: 0;
     display: flex;
-    margin: 63px 0 0 43px;
+    margin: 64px 0 0 43px;
     align-items: center;
     /deep/.el-input {
       width: 682px;
@@ -398,7 +415,7 @@ section {
         div {
           flex: 1;
           p {
-            margin: 16px 0;
+            margin: 5px 0;
             color: #333333;
           }
         }
---
+++ pc/src/DMH/qis.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div id="myChart" :style="{width: '300px', height: '300px'}"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "hello",
+  data() {
+    return {};
+  },
+  mounted() {
+    this.drawLine();
+  },
+  methods: {
+    drawLine() {
+      // 基于准备好的dom，初始化echarts实例
+      let myChart = this.$echarts.init(document.getElementById("myChart"));
+      // 绘制图表
+      myChart.setOption({
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b}: {c} ({d}%)"
+        },
+        legend: {
+          orient: "vertical",
+          x: "left",
+          data: ["日租车辆", "月租车辆", "年租车辆", "损坏车辆", "可用车辆"]
+        },
+        series: [
+          {
+            name: "访问来源",
+            type: "pie",
+            radius: ["50%", "70%"],
+            avoidLabelOverlap: false,
+            label: {
+              normal: {
+                show: false,
+                position: "center"
+              },
+              emphasis: {
+                show: true,
+                textStyle: {
+                  fontSize: "30",
+                  fontWeight: "bold"
+                }
+              }
+            },
+            labelLine: {
+              normal: {
+                show: false
+              }
+            },
+            data: [
+              { value: 335, name: "日租车辆"},
+              { value: 310, name: "月租车辆" },
+              { value: 234, name: "年租车辆" },
+              { value: 135, name: "损坏车辆" },
+              { value: 1548, name: "可用车辆" }
+            ],
+            color:['#37A2DA', '#67E0E3', '#FFDB5C','#FF9F7F','#E062AE'],
+            
+          }
+        ]
+      });
+    }
+  }
+};
+</script>
+
+<style scoped lang="less">
+</style>
